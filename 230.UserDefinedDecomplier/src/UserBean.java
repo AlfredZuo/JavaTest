c3a4f4562e7fc8c6c21d29df68341062a68cf19e
mygodzj <mygodzuojun@163.com>
2019-10-30 01:38:47 +0800

    实现方法部分的反编译

--- 230.UserDefinedDecomplier/src/UserBean.java
+++ 230.UserDefinedDecomplier/src/UserBean.java
@@ -3,4 +3,18 @@ public class UserBean {
     private int age;
     protected String addr;
     boolean sex;
+
+    //登录
+    public boolean login(String username, String pwd){
+        if("admin".equals(name)&&"123".equals(pwd)){
+            System.out.println("欢迎回来");
+            return true;
+        }
+        return false;
+    }
+
+    //退出
+    public void logout(){
+        System.out.println("系统已经安全退出");
+    }
 }
--- 230.UserDefinedDecomplier/src/UserDefinedDeComplier.java
+++ 230.UserDefinedDecomplier/src/UserDefinedDeComplier.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class UserDefinedDeComplier {
@@ -9,6 +10,8 @@ public class UserDefinedDeComplier {
         //这里仅仅是一个测试,如果只是getFields就只能获取public方式的属性type
         //Field[] fs = c.getFields();
         Field[] fs = c.getDeclaredFields();
+        Method[] ms = c.getDeclaredMethods();
+        /*
         System.out.println(fs.length);
         System.out.println(fs[0].getName());
         System.out.println("==========================================");
@@ -25,24 +28,50 @@ public class UserDefinedDeComplier {
         System.out.println("==========================调整反编译输出形式==========================");
         System.out.println("==========================调整反编译输出形式==========================");
         System.out.println("==========================调整反编译输出形式==========================");
-
+        */
         /*
                 public class UserBean {
                     public String name;
                     private int age;
                     protected String addr;
                     boolean sex;
+                    public boolean login(String, String){};
+                    public void logout(){};
                 }
         */
 
         StringBuffer sb = new StringBuffer();
         sb.append(Modifier.toString(c.getModifiers())+" class "+ c.getSimpleName()+ " {\r\n");
+        //属性部分
         for(Field field:fs){
             sb.append("\t"+ Modifier.toString(field.getModifiers()))
                     .append(Modifier.toString(field.getModifiers())==""? "":" ")                  //如果修饰符列表是缺省的，则不需要补充空格
                     .append(field.getType().getSimpleName()+" ")
                     .append(field.getName()+";\r\n");
         }
+        sb.append("\r\n");
+
+        //方法部分
+        for(Method method:ms){
+            sb.append("\t"+Modifier.toString(method.getModifiers()))
+                    .append(Modifier.toString(method.getModifiers())==""? "":" ")
+                    .append(method.getReturnType().getSimpleName()+" ")
+                    .append(method.getName()+"(");
+            //形式参数列表。由于处理方式不一样，建议按照老师的方法，使用for循环对逗号进行处理
+            Class[] parameterTypes = method.getParameterTypes();
+
+            //这里尽量不要用for(Class ps:parameterTypes)这样的循环，没法特殊处理逗号
+            for(int i = 0 ; i < parameterTypes.length ; i++){
+                if(i == parameterTypes.length-1){
+                    sb.append(parameterTypes[i].getSimpleName());
+                }else{
+                    sb.append(parameterTypes[i].getSimpleName()+",");
+                }
+            }
+
+            sb.append("){}\r\n");
+        }
+
         sb.append("}\r\n");
         System.out.println(sb);
 
